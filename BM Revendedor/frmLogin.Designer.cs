d09aae46d115d0b5c49ab3581cf1a60323e1d2f0
Yago <modenayago@yahoo.com>
2023-02-01 20:28:55 -0300

    Inicio tela de login Falta BD + Código

---
+++ BM Revendedor/frmLogin.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace BM_Revendedor
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.chcExibirSenha = new System.Windows.Forms.CheckBox();
+            this.btnCriarConta = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnDuvida = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lbVersão = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.panel1.Controls.Add(this.btnDuvida);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(343, 392);
+            this.panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Arial", 30F);
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(97, 165);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 57);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Login";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(381, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(193, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Faça seu Login Aqui!";
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Font = new System.Drawing.Font("Arial", 10F);
+            this.txtSenha.Location = new System.Drawing.Point(363, 164);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(232, 27);
+            this.txtSenha.TabIndex = 3;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLogin.Location = new System.Drawing.Point(363, 223);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(232, 43);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(359, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 23);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Usuário";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(359, 138);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 23);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Senha";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 10F);
+            this.label5.Location = new System.Drawing.Point(403, 288);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(159, 19);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Não tem uma conta?";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Font = new System.Drawing.Font("Arial", 10F);
+            this.txtUsuario.Location = new System.Drawing.Point(363, 105);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(232, 27);
+            this.txtUsuario.TabIndex = 8;
+            // 
+            // chcExibirSenha
+            // 
+            this.chcExibirSenha.AutoSize = true;
+            this.chcExibirSenha.Font = new System.Drawing.Font("Arial", 8F);
+            this.chcExibirSenha.Location = new System.Drawing.Point(490, 197);
+            this.chcExibirSenha.Name = "chcExibirSenha";
+            this.chcExibirSenha.Size = new System.Drawing.Size(105, 20);
+            this.chcExibirSenha.TabIndex = 9;
+            this.chcExibirSenha.Text = "exibir senha";
+            this.chcExibirSenha.UseVisualStyleBackColor = true;
+            // 
+            // btnCriarConta
+            // 
+            this.btnCriarConta.BackColor = System.Drawing.Color.Transparent;
+            this.btnCriarConta.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCriarConta.FlatAppearance.BorderSize = 0;
+            this.btnCriarConta.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCriarConta.Font = new System.Drawing.Font("Arial", 11F);
+            this.btnCriarConta.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.btnCriarConta.Location = new System.Drawing.Point(363, 310);
+            this.btnCriarConta.Name = "btnCriarConta";
+            this.btnCriarConta.Size = new System.Drawing.Size(232, 43);
+            this.btnCriarConta.TabIndex = 10;
+            this.btnCriarConta.Text = "Criar uma conta";
+            this.btnCriarConta.UseVisualStyleBackColor = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 10F);
+            this.label6.Location = new System.Drawing.Point(12, 350);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(195, 19);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Está com alguma dúvida?";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnDuvida
+            // 
+            this.btnDuvida.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnDuvida.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDuvida.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDuvida.Font = new System.Drawing.Font("Arial", 10F);
+            this.btnDuvida.ForeColor = System.Drawing.Color.Black;
+            this.btnDuvida.Location = new System.Drawing.Point(213, 340);
+            this.btnDuvida.Name = "btnDuvida";
+            this.btnDuvida.Size = new System.Drawing.Size(110, 43);
+            this.btnDuvida.TabIndex = 11;
+            this.btnDuvida.Text = "Clique Aqui!";
+            this.btnDuvida.UseVisualStyleBackColor = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial", 8F);
+            this.label7.Location = new System.Drawing.Point(507, 367);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(55, 16);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Versão:";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbVersão
+            // 
+            this.lbVersão.AutoSize = true;
+            this.lbVersão.Font = new System.Drawing.Font("Arial", 8F);
+            this.lbVersão.Location = new System.Drawing.Point(568, 367);
+            this.lbVersão.Name = "lbVersão";
+            this.lbVersão.Size = new System.Drawing.Size(27, 16);
+            this.lbVersão.TabIndex = 13;
+            this.lbVersão.Text = "1.0";
+            this.lbVersão.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(614, 392);
+            this.Controls.Add(this.lbVersão);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btnCriarConta);
+            this.Controls.Add(this.chcExibirSenha);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "frmLogin";
+            this.Text = "frmLogin";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnDuvida;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.CheckBox chcExibirSenha;
+        private System.Windows.Forms.Button btnCriarConta;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lbVersão;
+    }
+}
